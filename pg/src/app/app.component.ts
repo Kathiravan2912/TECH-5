f6ad20771c8b1b8e2d8932ea1d8ce5bb2088c3e4
Kathiravan A <kathiravan2002anbarasan@gmail.com>
2024-12-23 16:34:41 +0530

    student component added

--- pg/src/app/app.component.ts
+++ pg/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-// import { RouterOutlet,RouterLink, RouterLinkActive } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
-// import { LoginPageComponent } from "./login-page/login-page.component";
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HomeComponent],
+  imports: [CommonModule, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- pg/src/app/app.routes.ts
+++ pg/src/app/app.routes.ts
@@ -1,8 +1,16 @@
-import { Routes,RouterModule } from '@angular/router';
+import { Routes,RouterModule,RouterLink } from '@angular/router';
+import { NgModule } from '@angular/core';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student/student.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginPageComponent },
+    { path: 'student', component: StudentComponent }
 ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
